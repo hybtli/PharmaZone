8347f36dc833118616d6a3e4e1966fdaf772fca2
Nurbala Heybatov <nurbala788788@gmail.com>
2024-08-08 19:20:51 +0400

    .env file is changed

--- src/api/DutyPharmacyController.ts
+++ src/api/DutyPharmacyController.ts
@@ -1,51 +1,57 @@
-import {GetDistricts, GetDutyPharmacyDetail, GetProvinces} from "./DutyPharmacyController.types";
+import {
+  GetDistricts,
+  GetDutyPharmacyDetail,
+  GetProvinces,
+} from "./DutyPharmacyController.types";
 import axios from "axios";
 
-
 function DutyPharmacyController() {
-    return {
-        getPharmacies: async (city: string, county: string): Promise<GetDutyPharmacyDetail[]> => {
-            const options = {
-                method: 'GET',
-                url: 'https://www.nosyapi.com/apiv2/pharmacy',
-                params: {city: city, county: county},
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer R1gfU0BygA7H4cMKTCOs5EfNS0HmKOIt1ORfkAHZIkmeGDVUUSME7f0tMh66'
-                }
-            };
-
-            const response = await axios.request(options);
-            return response.data.data;
+  return {
+    getPharmacies: async (
+      city: string,
+      county: string,
+    ): Promise<GetDutyPharmacyDetail[]> => {
+      const options = {
+        method: "GET",
+        url: "https://www.nosyapi.com/apiv2/pharmacy",
+        params: { city: city, county: county },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NOISY_API_TOKEN}`,
         },
-        getProvinces: async (): Promise<GetProvinces[]> => {
-            const cityRequest = {
-                method: 'GET',
-                url: 'https://www.nosyapi.com/apiv2/pharmacy/city',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer R1gfU0BygA7H4cMKTCOs5EfNS0HmKOIt1ORfkAHZIkmeGDVUUSME7f0tMh66'
-                }
-            };
+      };
 
-            const response = await axios.request(cityRequest);
-            return response.data.data;
+      const response = await axios.request(options);
+      return response.data.data;
+    },
+    getProvinces: async (): Promise<GetProvinces[]> => {
+      const cityRequest = {
+        method: "GET",
+        url: "https://www.nosyapi.com/apiv2/pharmacy/city",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NOISY_API_TOKEN}`,
         },
-        getDistricts: async (city: string): Promise<GetDistricts[]> => {
-            const districtRequest = {
-                method: 'GET',
-                url: 'https://www.nosyapi.com/apiv2/pharmacy/city',
-                params: {city: city},
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer R1gfU0BygA7H4cMKTCOs5EfNS0HmKOIt1ORfkAHZIkmeGDVUUSME7f0tMh66'
-                }
-            };
+      };
 
-            const response = await axios.request(districtRequest);
-            return response.data.data;
+      const response = await axios.request(cityRequest);
+      return response.data.data;
+    },
+    getDistricts: async (city: string): Promise<GetDistricts[]> => {
+      const districtRequest = {
+        method: "GET",
+        url: "https://www.nosyapi.com/apiv2/pharmacy/city",
+        params: { city: city },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NOISY_API_TOKEN}`,
         },
-    }
+      };
+
+      const response = await axios.request(districtRequest);
+      return response.data.data;
+    },
+  };
 }
 
 export default DutyPharmacyController;
